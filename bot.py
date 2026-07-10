d3bbdc75cfc6b9d1e2db3256d975f570cb23824e
Aborayan828 <warvenbarwarii154@gmail.com>
2026-07-10 15:25:26 +0300

    Modify source channel and implement card pattern check
    Updated source channel and added card validation.

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 from telethon import TelegramClient, events
 from telethon.sessions import StringSession
+import re
 
 # ========== ڕێکخستنەکان ==========
 api_id = 33774652
@@ -7,21 +8,22 @@ api_hash = "c438941d8f43a0ff59fcc4b3f3c2fb42"
 session = "1ApWapzMBu55FFH32tbL7Qe15_IRzlscaRYhiHz_QGKffwE-uEKyG1tZ4-UiC5djhP8OnwIbPXlxPMM9oAM1ILtMK07BaLyOGkPjwtTU0oEaqsawX3w32enHteG1QjbFzBRoAV5rU-DiJmfqC305ZagiNOA8FAD4_LHMciBXldNTnA_y3mHlo9cpECRpZDrSa5QiSmxJcIKZDpy1luABObuVeiUr-hetJhk402ZmFJ2Iky-yqT_dBwaYberLfdQqTyQQoNWpIrwHt0bXsL6UqJCRgnjUGYb5iqMC5arNfofRvCKFGZwoFXWPHrOpYKnvTnnV1fi23tJ_UqJFIk5JLgZ03Nv3xJ3c="
 # ===================================
 
-SOURCE_CHANNELS = ["@SlimeChkGroup"]   # گروپی سەرچاوە
-TARGET_CHANNEL = "@cciraq73"           # گروپی تۆ
+SOURCE_CHANNEL = "@xforcegroupBOT"
+TARGET_CHANNEL = "@cciraq73"
 
 client = TelegramClient(StringSession(session), api_id, api_hash)
 
-@client.on(events.NewMessage(chats=SOURCE_CHANNELS))
+@client.on(events.NewMessage(chats=SOURCE_CHANNEL))
 async def handler(event):
     msg = event.message
     text = msg.text or ""
 
-    # **پشکنین: تەنها پەیامەکانی "Result" یان "APPROVED/DECLINED"**
-    if "Result" not in text and "APPROVED" not in text and "DECLINED" not in text:
-        return  # پشتگوێ بخرێت
+    # پشکنینی کارت
+    card_pattern = r'(\d{16,19})\|(\d{2})\|(\d{2,4})\|(\d{3,4})'
+    if not re.search(card_pattern, text):
+        return
 
-    # گۆڕینی ناوەکان (ئەگەر پێویست بوو)
+    # گۆڕینی ناوەکان
     new_text = text
     new_text = new_text.replace("@About_Warnisx", "@warven_24")
     new_text = new_text.replace("@Warnisx", "@warven_24")
