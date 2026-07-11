b8bd29946a8b9738e58f9dcb5e350a82b5f82030
Aborayan828 <warvenbarwarii154@gmail.com>
2026-07-11 22:02:42 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,12 +4,12 @@ from telethon.sessions import StringSession
 # ========== ڕێکخستنەکان ==========
 api_id = 33774652
 api_hash = "c438941d8f43a0ff59fcc4b3f3c2fb42"
-session = session = "1ApWapzMBu0hPg4YXGIf6GL6fgSZ0l9ZSVH48MQUpR7dJuFJSBm767Y71HZkek3MiY1Ib4Id3hZMxfMJdGWAbh0Rtbo3ZAPpMJ83NdinnPtRhckad5aU7hzKqBuAsQQZoJtITGEcBWHqFPtWViBeDjPWirMiGB05kUj6BHeKUGfqRLcYPbGMs-lxp9rjVswU67s0LrdDcp-qa0adD1T1Tc9Bhn44DujwzGtK3JW197hk0j1RTCzyRgrQV0nku4Y0-AWJLnZhnnjqNLXQAiWOphSUl2x6hyqQDOpKQXhJ6_xizfnlklUlAACGAqnsajTQ08zSP40mY61ZHisF5PCg8cjknPFXSQLY="
+session = "1ApWapzMBuz9KAAbVRLRzD6z7UujEu-bAOqcYCNgL_nfsG-FbXTnrL_-61YzdgcRhlUDduYA-P57mQcokdZGa2kA6qTbt0jKKb3-smWNzS5xk2bGrJ-C2d_QRoQqHPwzaj4I1mi4QV_4aNdmRIX7fCEL0AZotn_PZNmf-uHcSxRpLU3xuNICr1T59bl_M6vOzcyS_dayFznUKnI0oQVhEX8ICva1s98_uaSzXiA76ccNZCudbK1gO-ONKkXD0XYy5m0xTsZDJcZS6Cl35Yte-WDDgA2vovpcFKcK_soX2thFWdVlKL0yHxJVd0b3O062xA-92bNxI5uakXa64rdQ5Udgzge2gBKA="
 # ===================================
 
 SOURCE_CHANNEL = "@xforcegroupBOT"   # گروپی سەرچاوە
-TARGET_CHANNEL = "@CVC428"           # گروپی ئامانج (گۆڕدرا)
-TARGET_ADMIN = "@CC_posterBOT"       # ناوی ئەو بۆتەی کە دەتەوێت پەیامەکانی کۆپی بکەیت
+TARGET_CHANNEL = "@CVC428"           # گروپی ئامانج
+TARGET_ADMIN = "@CC_posterBOT"       # ناوی بۆتی تایبەت
 
 client = TelegramClient(StringSession(session), api_id, api_hash)
 
@@ -18,14 +18,12 @@ async def handler(event):
     msg = event.message
     text = msg.text or ""
 
-    # پشکنین: تەنها پەیامێک کە لەلایەن TARGET_ADMINـەوە نێردراوە
     if not event.message.sender:
         return
     sender_username = event.message.sender.username
     if sender_username != "CC_posterBOT":
         return
 
-    # گۆڕینی ناوەکان (بەپێی خواستی تۆ)
     new_text = text
     new_text = new_text.replace("CC POSTER bot", "Warven Scrapper")
     new_text = new_text.replace("@CCsPoster", "@CVC428")
